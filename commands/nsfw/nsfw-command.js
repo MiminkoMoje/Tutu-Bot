244c3ad9dc0a10eedb3ae9567bc8acc8c1bc5c23
Vasilis <vasilakis.lolipakis@gmail.com>
2021-07-10 01:17:14 +0300

    no longer needed

--- commands/nsfw/nsfw-command.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-    this.nsfwCommand = function (message, author, post, title, rAuthor, subreddit) {
-        message.channel.send(`Tutu well, **${author}** 💜\n${post}\n**${title}**\n*by ${rAuthor} in ${subreddit}*`)
-    };
-}
