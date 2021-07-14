d4f6f3b797ec803c104645b60436f5b72b7adabb
Vasilis <vasilakis.lolipakis@gmail.com>
2021-07-14 18:35:15 +0300

    renamed file

--- commands/general/memes.js
+++ commands/general/memes.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'dankmemes',
     aliases: ['dm', 'meme', 'memes', 'dank', 'funny'],
-    description: 'Shows random post of r/dankmemes.',
+    description: 'Shows random post of r/dankmemes, r/memes.',
     async execute(message, args) {
         const subreddit = ['memes', 'dankmemes']
         var rType = 'random-predefined-image'
