9bf55173d82c6ee6fe5280ab2aa2f89f68e03f6d
Vasilis <vasilakis.lolipakis@gmail.com>
2021-07-14 18:36:04 +0300

    array

--- commands/general/elephants.js
+++ commands/general/elephants.js
@@ -2,10 +2,10 @@ require(`${require.main.path}/commands/general/reddit.js`)();
 module.exports = {
     name: 'elephant',
     aliases: ['elephants', 'babyelephantgifs'],
-    description: 'Shows a random Reddit post from your selected subreddit.',
+    description: 'Shows random post of r/babyelephantgifs.',
     //guildOnly: true,
     async execute(message, args) {
-        var subreddit = 'babyelephantgifs'
+        var subreddit = ['babyelephantgifs']
         var rType = 'random-predefined-image'
         redditGetPost(args, message, subreddit, rType)
     }
