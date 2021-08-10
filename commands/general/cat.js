491e3af4e4adaa23ee2a53918e60f65ae910947a
Vasilis <vasilakis.lolipakis@gmail.com>
2021-08-10 21:23:39 +0300

    now using only reddit

--- commands/general/cat.js
+++ commands/general/cat.js
@@ -1,27 +1,10 @@
-const fetch = require('node-fetch');
-const nekoclient = require('nekos.life');
-const neko = new nekoclient();
 module.exports = {
   name: 'cat',
-  aliases: ['kitty', 'pussy', 'kitten', 'meow', ' moew', 'catto'],
-  description: 'Shows a random picture of a cat.',
-  async execute(message) {
-    if (Math.floor(Math.random() * 2) === 0) {
-      var { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-    } else {
-      const meowUrl = await neko.sfw.meow();
-      var file = meowUrl.url
-    }
-    const embed = {
-      "color": tutuColor,
-      "footer": {
-        "icon_url": message.author.avatarURL(),
-        "text": `Requested by ${message.author.tag} ${tutuEmote}`,
-      },
-      "image": {
-        "url": file
-      },
-    };
-    message.channel.send({ embed });
+  aliases: ['kitty', 'pussy', 'kitten', 'meow', ' moew', 'catto', 'cats', 'kittens'],
+  description: 'Shows random post of r/cats',
+  async execute(message, args) {
+    const subreddit = ['cats']
+    var rType = 'random-predefined-image'
+    redditGetPost(args, message, subreddit, rType)
   },
 };
