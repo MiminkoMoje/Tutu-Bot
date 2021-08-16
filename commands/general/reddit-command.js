712db850471163f62cd123e46eb6238e47ddf3e7
Vasilis <vasilakis.lolipakis@gmail.com>
2021-08-16 19:30:23 +0300

    removed prefix

--- commands/general/reddit-command.js
+++ commands/general/reddit-command.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const { prefix } = require(`${require.main.path}/config.json`);
 module.exports = {
   name: 'reddit',
   aliases: ['sub', 'submission', 'subreddit'],
@@ -10,10 +9,10 @@ module.exports = {
         .setTitle(`Commands`)
         .setFooter(`Requested by ${message.author.tag} ${tutuEmote} | by Vasilis#1517`, message.author.avatarURL())
         .addFields(
-          { name: `${prefix}random [subreddit]`, value: 'Shows a random submission from a subreddit' },
-          { name: `${prefix}top [subreddit] (hour/day/week/month/year/all)`, value: 'Shows the top posts of a subreddit' },
-          { name: `${prefix}user [a Reddit user]`, value: 'Shows posts of a Reddit user' },
-          { name: `${prefix}id [a Reddit post ID]`, value: 'Gets a post from its ID' },
+          { name: `random [subreddit]`, value: 'Shows a random submission from a subreddit' },
+          { name: `top [subreddit] (hour/day/week/month/year/all)`, value: 'Shows the top posts of a subreddit' },
+          { name: `user [a Reddit user]`, value: 'Shows posts of a Reddit user' },
+          { name: `id [a Reddit post ID]`, value: 'Gets a post from its ID' },
         )
       message.channel.send({embeds: [helpEmbed]});
   }
