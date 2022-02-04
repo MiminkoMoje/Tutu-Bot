a1e76aa8c85bbff7c82772772cbbcdbc58e5ca64
Vasilis <vasilakis.lolipakis@gmail.com>
2022-02-04 16:14:34 +0200

    searches on all subreddits

--- commands/general/search.js
+++ commands/general/search.js
@@ -1,26 +1,32 @@
 require(`${require.main.path}/events/embeds.js`)();
 module.exports = {
-  name: 'search',
-  aliases: ['s', 'find'],
-  description: 'Conducts a search.',
+  name: "search",
+  aliases: ["s", "find"],
+  description: "Conducts a search.",
   async execute(message, args) {
-    if (args[0]) {
-      var subreddit = args[0]
-    } else {
-      const errorMsg = `Please provide a subreddit to conduct the search on.\n\nRemember, the correct syntax is: **search [subreddit] [query]**.`
-      return errorEmbed(message, errorMsg, message.author.avatarURL(), message.author.tag)
+
+    if (!args[0]) {
+      const errorMsg = `Please provide a search query.`;
+      return errorEmbed(
+        message,
+        errorMsg,
+        message.author.avatarURL(),
+        message.author.tag
+      );
     }
 
-    if (!args[1]) {
-      const errorMsg = `Please provide a search query.\n\nRemember, the correct syntax is: **search [subreddit] [query]**.`
-      return errorEmbed(message, errorMsg, message.author.avatarURL(), message.author.tag)
-    }
+    //delete args[0];
+    var query = args.join(" ");
+    var rType = "search";
 
-    delete args[0]
-    var query = args.join(' ').substring(1)
-    var rType = 'search'
-
-    redditGetPost(args, message, subreddit, rType, time = 'all', query, sort = 'relevance')
-
+    redditGetPost(
+      args,
+      message,
+      (subreddit = "all"),
+      rType,
+      (time = "all"),
+      query,
+      (sort = "hot")
+    );
   },
 };
